25143de67b5a37afdca75644a5cb597fe90bea44
KristiyanSevov <kristiyan.sevov@gmail.com>
2018-05-21 18:03:44 +0300

    Added exercise

--- Exercises/04. Linear Data Structures (Exercise)/src/Ex9_MaximumElement.java
+++ Exercises/04. Linear Data Structures (Exercise)/src/Ex9_MaximumElement.java
@@ -33,4 +33,41 @@ public class Ex9_MaximumElement {
         }
         System.out.print(sb);
     }
+
+//    //the other way
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(reader.readLine());
+//        ArrayDeque<Integer> stack = new ArrayDeque<>();
+//        StringBuilder sb = new StringBuilder();
+//        int max = Integer.MIN_VALUE;
+//        for (int i = 0; i < n; i++) {
+//            String[] inputs = reader.readLine().split(" ");
+//            String command = inputs[0];
+//            switch (command) {
+//                case "1":
+//                    int elem = Integer.parseInt(inputs[1]);
+//                    stack.push(elem);
+//                    if (elem > max) {
+//                        max = elem;
+//                    }
+//                    break;
+//                case "2":
+//                    if(stack.pop() == max){
+//                        max = Integer.MIN_VALUE;
+//                        for (int num : stack) {
+//                            if (num > max) {
+//                                max = num;
+//                            }
+//                        }
+//                    }
+//                    break;
+//                case "3":
+//                    sb.append(max);
+//                    sb.append("\n");
+//                    break;
+//            }
+//        }
+//        System.out.print(sb);
+//    }
 }
